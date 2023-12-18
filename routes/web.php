608a06dbb3476840b3b0eda1ec370dab632aca3c
johnvees <yohanesvelly23@gmail.com>
2023-12-19 04:02:44 +0700

    feat: slicing login and sign up

--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,20 @@ Route::get('/', function () {
     ]);
 });
 
+Route::redirect('/', '/prototype/login');
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::prefix('prototype')->name('prototype.')->group(function () {
+    route::get('/login', function () {
+        return Inertia::render('Prototype/Login');
+    })->name('login');
+    route::get('/register', function () {
+        return Inertia::render('Prototype/Register');
+    })->name('register');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
